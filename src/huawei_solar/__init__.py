1c68327c84c6477772a19e7e789a769fef6e4dcc
Emil Vanherp <emil@vanherp.me>
2021-01-13 22:07:26 +0100

    bump version

--- src/huawei_solar/__init__.py
+++ src/huawei_solar/__init__.py
@@ -1,6 +1,6 @@
 """
 interact async with the huawei inverter using modbus to get production data
 """
-__version__ = "0.0.11"
+__version__ = "0.1.0b2"
 
 from .huawei_solar import *  # noqa: F401 F403
