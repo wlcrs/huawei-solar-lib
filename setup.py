10e4a2476bf986841cbe48af670a6a211540568d
Emil Vanherp <emil@vanherp.me>
2021-03-28 21:50:47 +0200

    add missing pytz dependency to setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/EmilV2/huawei-solar",
-    install_requires=["pymodbus>=2.4.0,<3.0"],
+    install_requires=["pymodbus>=2.4.0,<3.0", "pytz>=2019.3"],
     python_requires=">=3.6",
     packages=["huawei_solar"],
     package_dir={"": "src"},
