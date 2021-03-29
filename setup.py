943e25733fb2eb0f8d9f04410e8e5a1e762ba493
Emil Vanherp <emil@vanherp.me>
2021-03-29 10:40:56 +0200

    wip for better async support

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/EmilV2/huawei-solar",
-    install_requires=["pymodbus>=2.4.0,<3.0", "pytz>=2019.3"],
+    install_requires=["git+https://github.com/Emilv2/pymodbus@3bdb32", "pytz>=2019.3"],
     python_requires=">=3.6",
     packages=["huawei_solar"],
     package_dir={"": "src"},
