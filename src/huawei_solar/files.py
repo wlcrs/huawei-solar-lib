376944731dca3d6c74fd92b09a565d89baa43066
wlcrs <wlcrs@users.noreply.github.com>
2022-12-26 10:09:29 +0100

    Clarification

--- src/huawei_solar/files.py
+++ src/huawei_solar/files.py
@@ -101,7 +101,7 @@ class OptimizerRealTimeDataFile:
         if len(file_data) < struct.calcsize(OptimizerRealTimeDataFile.HEADER):
             return
 
-        self.file_version = struct.unpack_from(OptimizerRealTimeDataFile.HEADER, file_data, offset)
+        (self.file_version, reserved) = struct.unpack_from(OptimizerRealTimeDataFile.HEADER, file_data, offset)
         offset += struct.calcsize(OptimizerRealTimeDataFile.HEADER)
 
         has_next_optimizer_data_unit = True
@@ -311,7 +311,7 @@ class OptimizerSystemInformationDataFile:  # pylint: disable=too-few-public-meth
                     )
                 )
         else:
-            raise HuaweiSolarException("Only V102 and V103 file format are supported.")
+            raise HuaweiSolarException(f"Unsupported OptimizerSystemInformation file version: {self.file_version}")
 
 
 def _to_string(data: bytes):
